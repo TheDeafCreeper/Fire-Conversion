577a21d07c1b08b7b6a7e3e50a02e75e399c8cb1
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2023-02-01 10:08:19 -0600

    Initial commit

---
+++ src/main/java/com/thedeafcreeper/fireconversion/Fireconversion.kt
@@ -0,0 +1,13 @@
+package com.thedeafcreeper.fireconversion
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FireConversion : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
